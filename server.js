53592f53d8946a84dd49a4ad98e986b14a21655e
Fajar Sujai <fajar.s.fratama@gmail.com>
2022-04-01 10:11:20 +0700

    update serverjs

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const todoRoutes = express.Router();
 const PORT = 4000;
-const client = require('prom-client')
+// const client = require('prom-client')
 
 let Todo = require('./todo.model');
 
@@ -69,47 +69,47 @@ todoRoutes.route('/add').post(function(req, res) {
 
 app.use('/todos', todoRoutes);
 
-// // App
-// // const client = require('prom-client');
-// const collectDefaultMetrics = client.collectDefaultMetrics;
-// // Probe every 5th second.
-// collectDefaultMetrics({ timeout: 5000 });
+// App
+const client = require('prom-client');
+const collectDefaultMetrics = client.collectDefaultMetrics;
+// Probe every 5th second.
+collectDefaultMetrics({ timeout: 5000 });
 
-// const counter = new client.Counter({
-//   name: 'node_request_operations_total',
-//   help: 'The total number of processed requests'
-// });
+const counter = new client.Counter({
+  name: 'node_request_operations_total',
+  help: 'The total number of processed requests'
+});
 
-// const histogram = new client.Histogram({
-//   name: 'node_request_duration_seconds',
-//   help: 'Histogram for the duration in seconds.',
-//   buckets: [1, 2, 5, 6, 10]
-// });
+const histogram = new client.Histogram({
+  name: 'node_request_duration_seconds',
+  help: 'Histogram for the duration in seconds.',
+  buckets: [1, 2, 5, 6, 10]
+});
 
-// // const app = express();
-// // app.get('/', (req, res) => {
+// const app = express();
+app.get('/hello', (req, res) => {
 
-// //   //Simulate a sleep
-// //   var start = new Date()
-// //   var simulateTime = 1000
+  //Simulate a sleep
+  var start = new Date()
+  var simulateTime = 1000
 
-// //   setTimeout(function(argument) {
-// //     // execution time simulated with setTimeout function
-// //     var end = new Date() - start
-// //     histogram.observe(end / 1000); //convert to seconds
-// //   }, simulateTime)
+  setTimeout(function(argument) {
+    // execution time simulated with setTimeout function
+    var end = new Date() - start
+    histogram.observe(end / 1000); //convert to seconds
+  }, simulateTime)
 
-// //   counter.inc();
+  counter.inc();
   
-// //   res.send('Hello world\n');
-// // });
+  res.send('Hello world\n');
+});
 
 
-// // Metrics endpoint
-// app.get('/metrics', (req, res) => {
-//   res.set('Content-Type', client.register.contentType)
-//   res.end(client.register.metrics())
-// })
+// Metrics endpoint
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', client.register.contentType)
+  res.end(client.register.metrics())
+})
 
 
 
